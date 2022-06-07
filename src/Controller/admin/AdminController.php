2e711b9517caa93cc78a2ef723398b490cac7da5
Mickael Desbiens <desbiens.mickael01@gmail.com>
2022-06-07 22:25:54 +0200

    added admin view and scss setting

---
+++ src/Controller/admin/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/', name: 'app_')]
+class AdminController extends AbstractController
+{
+    #[Route('admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}

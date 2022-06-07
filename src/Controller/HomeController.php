d66ebf6b6a136556d0d1da8df83556e00b417598
Mickael Desbiens <desbiens.mickael01@gmail.com>
2022-06-07 22:06:13 +0200

    scss setting

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/', name: 'app_')]
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig');

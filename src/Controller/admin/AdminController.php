6933b520d9a10f91fab0ff5d2dbb75277d6bb48a
Mickael Desbiens <desbiens.mickael01@gmail.com>
2022-06-15 11:01:44 +0200

    securing the admin route and fix scss

--- src/Controller/admin/AdminController.php
+++ src/Controller/admin/AdminController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/', name: 'app_')]
+#[Route('/')]
 class AdminController extends AbstractController
 {
     #[Route('admin', name: 'admin')]

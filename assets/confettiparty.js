926e042875f48e025d5f7059a496118e2f4a8893
Mélissa <melissa-callejon_student2022@wilder.school>
2022-06-22 11:41:09 +0200

    improvement of the confetti position

--- assets/confettiparty.js
+++ assets/confettiparty.js
@@ -1,16 +1,15 @@
 const confetti = require('canvas-confetti');
-const canvasConfetti = document.createElement('canvas');
-document.body.appendChild(canvasConfetti);
+const canvasConfetti = document.getElementById('confettis');
+
 const myConfetti = confetti.create(canvasConfetti, {
     resize: true,
     useWorker: true
 });
-const colors = ["#8b5642", "#6a696b"];
+const colors = ["#DD7110", "#6a696b"];
 const duration = 27 * 1000;
 const end = Date.now() + duration;
 
 window.gameWin = () => {
-    new Audio('/sounds/sound-win.mp3').play();
     window.launchConfetties();
 }
 window.launchConfetties = () => {

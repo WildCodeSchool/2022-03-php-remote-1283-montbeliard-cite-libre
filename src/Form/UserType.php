a32b6284c8654750249b4148604f57dbb0345808
Mickael Desbiens <desbiens.mickael01@gmail.com>
2022-06-27 12:17:45 +0200

    secure the modification of roles in the UserType set to Super Admin

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,37 +3,45 @@
 namespace App\Form;
 
 use App\Entity\User;
-use App\Entity\Classe;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+
         $builder
             ->add('username', TextType::class, [
                 'label' => "Nom d'utilisateur",
             ])
-            ->add('roles', ChoiceType::class, [
-                'label' => "Rôles",
-                'expanded' => true,
-                'multiple' => true,
-                'choices' => [
-                    'Administrateur' => 'ROLE_ADMIN',
-                    'Super administrateur' => 'ROLE_SUPER_ADMIN'
-                ]
-            ])
             ->add('password', null, [
                 'label' => 'Mot de passe',
             ])
-            ->add('classe', null, ['choice_label' => 'classe',])
-        ;
+            ->add('classe', null, ['choice_label' => 'classe',]);
+
+        if ($this->security->isGranted('ROLE_SUPER_ADMIN')) {
+            $builder
+                ->add('roles', ChoiceType::class, [
+                    'label' => "Rôles",
+                    'expanded' => true,
+                    'multiple' => true,
+                    'choices' => [
+                        'Administrateur' => 'ROLE_ADMIN',
+                        'Super administrateur' => 'ROLE_SUPER_ADMIN'
+                    ]
+                ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void

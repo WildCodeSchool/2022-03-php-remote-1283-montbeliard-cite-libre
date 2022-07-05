6fa511a04aa957551bf055b7121ac14441ec4cb0
Mickael Desbiens <desbiens.mickael01@gmail.com>
2022-07-05 21:57:51 +0200

    fix conflict

--- assets/app.js
+++ assets/app.js
@@ -9,42 +9,13 @@
 import './styles/app.scss';
 
 // import js
-<<<<<<< HEAD
-
-=======
 import './confettiparty';
 import './cardparchment';
 import './cardquestion';
 import './diceanimation';
->>>>>>> 3ac7840c7935651655b0d5fb4ee85f3c9bcd071f
 
 // start the Stimulus application
 import './bootstrap';
 
 require('bootstrap');
 
-<<<<<<< HEAD
-// import './cardparchment.js';
-// import './cardquestion.js';
-
-const modal = document.getElementById("myModal");
-const btn = document.querySelector('.btn-dice');
-const ingame = document.querySelector('.ingame');
-
-btn.addEventListener('click', () => {
-    setTimeout(showModal, 3000)
-})
-
-function showModal() {
-    modal.classList.remove('d-none');
-    ingame.style.opacity = '0.5'
-}
-
-let btnDisplayResponse = document.getElementById("btn-display-response");
-let response = document.getElementById("response");
-
-btnDisplayResponse.addEventListener("click", () => {
-    response.classList.toggle('d-none');
-});
-=======
->>>>>>> 3ac7840c7935651655b0d5fb4ee85f3c9bcd071f

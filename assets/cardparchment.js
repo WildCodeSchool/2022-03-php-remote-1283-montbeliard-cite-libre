f6a2197844f55dfa3214a86f18b923446bc51c41
unknown <ala.johan@gmail.com>
2022-07-06 19:32:39 +0200

    add button to close question modal and re open from the game

--- assets/cardparchment.js
+++ assets/cardparchment.js
@@ -1,6 +1,8 @@
 let btnDisplayResponse = document.getElementById("btn-display-response");
 let response = document.getElementById("response");
 
+const modal = document.getElementById("myModal");
+
 if (btnDisplayResponse) {
     btnDisplayResponse.addEventListener("click", () => {
         response.classList.toggle('d-none');
--- assets/diceanimation.js
+++ assets/diceanimation.js
@@ -1,11 +1,22 @@
 window.rollDice = (result) => {
-    let dice = document.getElementById('dice');
+    let closeModal = document.querySelector(".close-modal");
+
+    let dice = document.getElementById("dice");
     dice.dataset.side = result;
     dice.classList.toggle("reRoll");
     const modal = document.getElementById("myModal");
-    const ingame = document.querySelector('.ingame');
-    setTimeout(function() {
-        modal.classList.remove('d-none');
-        ingame.style.opacity = '0.5'
-    }, 2000)
-}
+    const ingame = document.querySelector(".ingame");
+    setTimeout(function () {
+        modal.classList.remove("d-none");
+        ingame.style.opacity = "0.5";
+    }, 2000);
+    closeModal.addEventListener("click", () => {
+        modal.classList.add("d-none");
+        ingame.style.opacity = "1";
+    });
+    const showModal = document.querySelector(".show-modal");
+    showModal.addEventListener("click", () => {
+        modal.classList.remove("d-none");
+        ingame.style.opacity = "0.5";
+    });
+};

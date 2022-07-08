7094324907ee1abbbd33b52bf711668e8252471b
unknown <ala.johan@gmail.com>
2022-07-08 19:17:22 +0200

    config live component

---
+++ src/Components/RollComponent.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\Components;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\Attribute\LiveArg;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+#[AsLiveComponent('roll')]
+class RollComponent extends AbstractController
+{
+    use DefaultActionTrait;
+}

3f1a8f7bb2081203b7e91e6ba245500360600f17
Jean-François Morin <jeanfrancois.morin@wildcodeschool.com>
2022-07-12 14:33:56 +0200

    add chrono

--- assets/controllers/game_controller.js
+++ assets/controllers/game_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static targets = ["modal", "bodyGame"];
+    static targets = ["modal", "bodyGame", "chrono", "chronoProgress"];
     openModal() {
         this.bodyGameTarget.classList.add("pointer-event-none");
         this.modalTarget.classList.remove("d-none");
@@ -9,10 +9,56 @@ export default class extends Controller {
     closeModal(event) {
         this.bodyGameTarget.classList.remove("pointer-event-none");
         this.modalTarget.classList.add("d-none");
+        this.modalTarget.classList.remove("card-parchment--modal-auto-show");
     }
-    openRoll() {
-        setTimeout(() => {
-            this.openModal();
-        }, 3000);
+
+    connect(event) {
+        const updateChrono = (seconds) => {
+            const duration = getCookie("duration") * 60;
+            if (seconds > 0) {
+                this.chronoTarget.innerText = new Date(seconds * 1000).toISOString().substr(11, 8)
+            } else {
+                this.chronoTarget.innerText = 'END OF GAME'
+                this.chronoTarget.classList.add("small")
+            }
+            this.chronoProgressTarget.style.width = `${(duration-seconds)*100/duration}%`
+        }
+        const getCookie = (cname) => {
+            let name = cname + "=";
+            let ca = document.cookie.split(';');
+            for (let i = 0; i < ca.length; i++) {
+                let c = ca[i];
+                while (c.charAt(0) === ' ') {
+                    c = c.substring(1);
+                }
+                if (c.indexOf(name) === 0) {
+                    return c.substring(name.length, c.length);
+                }
+            }
+            return "";
+        }
+        let timer;
+        const chronometer =  (pageLoaded = true) => {
+            let endedAt = new Date(getCookie("endedAt") * 1000);
+
+            let currentDate = new Date();
+            // calculate time before the end of laps in milliseconds
+            let endedIn = endedAt.getTime() - currentDate.getTime();
+
+            if (endedIn > 0) {
+                timer = setTimeout(function () {
+                    chronometer(false);
+                }, 1000)
+            } else {
+                clearTimeout(timer);
+                endedIn = 0;
+                if (!pageLoaded) {
+                    // window.location.href = "/testing/end";
+                }
+            }
+            updateChrono(Math.ceil(endedIn / 1000))
+        }
+
+        chronometer();
     }
 }

aef78dc20624b2dbf736a48bdb6ef2780b932ba1
Mickael Desbiens <desbiens.mickael01@gmail.com>
2022-07-14 10:13:45 +0200

    added a filter in the titles of the ranking table

--- src/Controller/ClassementController.php
+++ src/Controller/ClassementController.php
@@ -10,11 +10,19 @@ use Symfony\Component\Validator\Constraints\NotNull;
 
 class ClassementController extends AbstractController
 {
-    #[Route('/classement', name: 'app_classement')]
-    public function index(GameRepository $gameRepository): Response
+    #[Route('/classement/{filter}', name: 'classement')]
+    public function index(GameRepository $gameRepository, string $filter): Response
     {
         $games = $gameRepository->findByScore();
-
+        if ($filter === 'user') {
+            $games = $gameRepository->findByUser();
+        }
+        if ($filter === 'classe') {
+            $games = $gameRepository->findByClasse();
+        }
+        if ($filter === 'name') {
+            $games = $gameRepository->findBy([], ['name' => 'ASC']);
+        }
         return $this->render('classement/index.html.twig', [
             'controller_name' => 'ClassementController',
             'games' => $games,
